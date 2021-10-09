75de36e51f0ea6e407886045af3cd494c83cf64b
Chandler Cheng <dc9909@hotmail.com>
2021-10-09 14:05:58 +1300

    Setup Moto type dropdown field

--- app/src/main/java/com/cheng/transactionterminal/ui/CardEntryFragment.kt
+++ app/src/main/java/com/cheng/transactionterminal/ui/CardEntryFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.cheng.transactionterminal.R
 import com.cheng.transactionterminal.databinding.FragmentCardEntryBinding
+import com.cheng.transactionterminal.entity.MoToType
 import com.cheng.transactionterminal.presenter.CardEntryPresenter
 import java.lang.ref.WeakReference
 
@@ -16,9 +20,21 @@ class CardEntryFragment : Fragment() {
 
     private var _binding: FragmentCardEntryBinding? = null
 
+    private var motoTypeTextView: AutoCompleteTextView? = null
     private val presenter = CardEntryPresenter()
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
+    private val currentMoToType: MoToType?
+        get() {
+            motoTypeTextView?.apply {
+                return when (listSelection) {
+                    0 -> MoToType.Single
+                    1 -> MoToType.Recurring
+                    else -> null
+                }
+            }
+            return null
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +55,14 @@ class CardEntryFragment : Fragment() {
                 WeakReference(binding.inputLayoutExpiry)
             )
         )
+
+        motoTypeTextView = binding.autoCompleteTextViewMotoType
+        val motoTypeStrings = requireContext().resources.getStringArray(R.array.moto_type_values)
+        val motoTypeAdapter = ArrayAdapter(
+            requireContext(), android.R.layout.simple_dropdown_item_1line, motoTypeStrings
+        )
+        binding.autoCompleteTextViewMotoType.setAdapter(motoTypeAdapter)
+
         binding.buttonContinueTransaction.setOnClickListener {
             findNavController().navigate(R.id.action_to_first_fragment)
         }
